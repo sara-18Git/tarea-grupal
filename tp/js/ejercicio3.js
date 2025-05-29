6cf958060979e0e1b5a70efa1d843da85e40c556
Matias Eliseo Soria Indarte <matiassoriaindarte@gmail.com>
2025-05-28 21:30:20 -0300

    ejercicios 1 2 y 3

---
+++ tp/js/ejercicio3.js
@@ -0,0 +1,6 @@
+let text = "esto es un texto DE ejemplo JAVAscript";
+text = text.toLowerCase(); // Convierte todo a minúsculas
+console.log(
+  "el texto incluye el texto javascript?",
+  text.includes("javascript")
+); // Verifica si contiene "javascript"
---
+++ tp/js/ejercicios2.js
@@ -0,0 +1,4 @@
+let text = "texto De Ejemplo";
+
+console.log(text.toLowerCase()); // Convierte a minúsculas
+console.log(text.toUpperCase()); // Convierte a mayúsculas
